8cc4ff482101b8a44d391b07bdb45ce367bd7d37
GBTC441007UR <GBTC441007UR@MC.MONTGOMERYCOLLEGE.EDU>
2019-04-26 13:31:15 -0400

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,131 @@
+package com.company;
+import java.util.Random;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Iterator;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+Random generator = new Random();
+
+
+
+
+
+
+
+ArrayList<String> Rice = new ArrayList<String>();
+        Random R = new Random();
+        Rice.add("white rice");
+        Rice.add("brown rice");
+        Rice.add("no rice");
+
+ArrayList<String> Meat = new ArrayList<String>();
+        Random M = new Random();
+        Meat.add("chicken meat");
+        Meat.add("steak meat");
+        Meat.add("carnidas meat");
+        Meat.add("chorizo meat");
+        Meat.add("sofritas meat");
+        Meat.add("veggies meat");
+
+
+
+        ArrayList<String> Beans = new ArrayList<String>();
+        Random B = new Random();
+        Beans.add("pinto beans");
+        Beans.add("black beans");
+        Beans.add("no beans");
+
+ArrayList<String> Salsa = new ArrayList<String>();
+        Random S = new Random();
+        Salsa.add("mild salsa");
+        Salsa.add("medium salsa");
+        Salsa.add("hot salsa");
+        Salsa.add("no salsa");
+
+ArrayList<String> Veggies = new ArrayList<String>();
+        Random V= new Random();
+        Veggies.add("lettuce");
+        Veggies.add("fajita veggies");
+        Veggies.add("no veggies");
+
+
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+
+//veggies.get(V.next));
+
+
+
+
+
+       // System.out.println("Using for loop");
+       // for (int i = 0; i < Rice.size(); i++) {
+         //   System.out.println(Rice.get(i));
+
+System.out.println("Burrito 1:"+Rice.get(0)+", "+Meat.get(0)+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 2:"+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 3: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 4: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 5: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 6: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println(" Burrito 7: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 8: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 9: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+        Collections.shuffle(Rice);
+        Collections.shuffle(Meat);
+        Collections.shuffle(Beans);
+        Collections.shuffle(Salsa);
+        Collections.shuffle(Veggies);
+System.out.println("Burrito 10: "+Rice.get(0)+", "+Meat.get(0)+", "+Beans.get(0)+", "+Salsa.get(0)+", "+Veggies.get(0));
+
+	// write your code here
+        //system.out.println("Burrito 1 : " +rice, +meat, +beans, +salsa, +veggies)
+    }
+}
